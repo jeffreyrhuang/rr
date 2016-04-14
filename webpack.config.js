1dcf960e5a13f7622377d3ad0fbffe6c425b55fb
Jeffrey Huang <jeffreyrhuang@gmail.com>
2016-04-14 10:36:50 -0600

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+  entry:  [
+  	'./src/index.js'
+  ],
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    colors: true,
+    contentBase: './public',
+    inline: true
+  }
+}
